28b8dbf61a687d4961655653b115a89b83a43e91
Savetheinternet <savetheinternet@tinyboard.org>
2011-11-21 15:01:09 +1100

    h

--- http.sh
+++ http.sh
@@ -1,4 +1,17 @@
 http_header() {
-	echo $1
+	http_headers=( ["$1"]="$2")
+	
 }
 
+# print HTTP headers; start sending content.
+http_headers() {
+	echo "HTTP/1.1 $1 $2"
+	
+	for header in "${!http_headers[@]}"; do
+		echo "$header: ${http_headers[$header]}"
+	done
+	
+	echo ""
+}
+
+declare -A http_headers
--- ib.sh
+++ ib.sh
@@ -4,9 +4,14 @@
 . http.sh
 . functions.sh
 
+http_header "Content-Type" "text/html"
+
 # Stuff goes here.
 # 
 # ...
+
+http_headers 200 "OK"
+echo -n 'h'
 
 exit 0
 
