ab1dc57e0c7fcd3fa87033efbe1e81fab1e77e66
Savetheinternet <savetheinternet@tinyboard.org>
2011-12-07 23:30:33 +1100

    use openssl for tripcode hashing

--- strings.sh
+++ strings.sh
@@ -31,8 +31,7 @@ make_name_tripcode() {
 		salt=$(echo $salt | sed 's/[^\.-z]/./g')
 		salt=$(echo -n $salt | tr ':;<=>?@[\\]^_`' 'ABCDEFGabcdef')
 
-		# TODO: Add mcrypt support
-		tripcode=$(perl -e 'print crypt($ARGV[0], $ARGV[1])' $tripcode $salt)
+		tripcode=$(openssl passwd -crypt -salt "$salt" "$tripcode")
 		tripcode=$(echo -n $tripcode | tail -c 10)
 		
 		echo -n "!$tripcode"
