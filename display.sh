6ad81488d405e681143edee80dfbc12ed852b650
Savetheinternet <savetheinternet@tinyboard.org>
2011-12-08 14:33:15 +1100

    .

--- display.sh
+++ display.sh
@@ -1,12 +1,12 @@
 index() {
 	
 	cat << HTML
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<link rel="stylesheet" media="screen" href="/style.css" />
-	</head>
-	<body>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" media="screen" href="/style.css" />
+</head>
+<body>
 HTML
 
 	fetch_rows "SELECT * FROM posts ORDER BY time DESC" "post" | while read row; do
@@ -15,20 +15,20 @@ HTML
 		formatted_time=$(echo "${post['time']}" | awk '{print strftime("%c", $1)}')
 	
 	cat << HTML
-		<div class="post reply">
-			<p class="intro">
-				<span class="name">${post['name']}</span>
-				$formatted_time No.${post['id']}
-			</p>
-			<p>${post['body']}</p>
-		</div>
+	<div class="post reply">
+		<p class="intro">
+			<span class="name">${post['name']}</span>
+			$formatted_time No.${post['id']}
+		</p>
+		<p class="body">${post['body']}</p>
+	</div>
 HTML
 	
 	done
 
 	cat << HTML
-	</body>
-	</html>
+</body>
+</html>
 HTML
 	
 }
