857875aff70e5dd005b54a3090c5035ef30329a4
Savetheinternet <savetheinternet@tinyboard.org>
2011-12-08 14:30:26 +1100

    handle requests

---
+++ display.sh
@@ -0,0 +1,35 @@
+index() {
+	
+	cat << HTML
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<link rel="stylesheet" media="screen" href="/style.css" />
+	</head>
+	<body>
+HTML
+
+	fetch_rows "SELECT * FROM posts ORDER BY time DESC" "post" | while read row; do
+		eval $row
+	
+		formatted_time=$(echo "${post['time']}" | awk '{print strftime("%c", $1)}')
+	
+	cat << HTML
+		<div class="post reply">
+			<p class="intro">
+				<span class="name">${post['name']}</span>
+				$formatted_time No.${post['id']}
+			</p>
+			<p>${post['body']}</p>
+		</div>
+HTML
+	
+	done
+
+	cat << HTML
+	</body>
+	</html>
+HTML
+	
+}
+
--- ib.sh
+++ ib.sh
@@ -13,29 +13,37 @@ cd $(dirname "$scriptname")
 . functions.sh
 . database.sh
 . strings.sh
+. display.sh
 
-http_header "Content-Type" "text/html; charset=utf-8"
+http_header "Content-Type" "text/plain; charset=utf-8"
 http_header "X-Powered-By" "IB"
 
 # Stuff goes here.
 # 
 # ...
 
-http_headers 200 "OK"
-
 declare -A row
 
-fetch_rows "SELECT * FROM posts ORDER BY time DESC" "post" | while read row; do
-	eval $row
-	
-	formatted_time=$(echo "${post['time']}" | awk '{print strftime("%c", $1)}')
-	echo -n	'<div class="post">'
-	echo -n '<span class="name">'"${post['name']}"'</span>' \
-		"$formatted_time" \
-		"No.${post['id']}"
-	echo -n "<p>${post['body']}</p>"
-	echo -n '</div>'
-done
+case $REQUEST_URI in
+	"/" )
+		http_header "Content-Type" "text/html; charset=utf-8"
+		http_headers 200 "OK"
+		
+		# show threads on page 1
+		index 1
+		
+		;;
+	"/style.css" )
+		http_header "Content-Type" "text/css"
+		http_headers 200 "OK"
+		
+		cat style.css
+		
+		;;
+	* )
+		http_headers 404 "Not Found"
+		echo -n '404 Not Found'
+esac
 
 exit 0
 
